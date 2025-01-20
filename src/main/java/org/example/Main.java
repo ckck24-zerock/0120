84741fe1f31741d5b9a434bd2d6cbc43989a245d
zerock <kangckck24@gmail.com>
2025-01-20 10:42:38 +0900

    1st

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -12,8 +13,12 @@ public class Main {
     //bad code
     public static void main(String[] args) throws Exception{
 
+        String path ="https://image6.coupangcdn.com/image/vendor_inventory/8130/a5332057106c049b765ac100b3ee679700895a09a35a38820e3e10367097.jpg";
+        URL url = new URL(path);
+        InputStream in = url.openStream();
+
         //Menu m = new PizzaMenu();
-        InputStream in = new FileInputStream("C:\\zzz\\aaa.jpg");
+        //InputStream in = new FileInputStream("C:\\zzz\\aaa.jpg");
         OutputStream out = new FileOutputStream("C:\\zzz\\copy.jpg");
 
         //모든 빨대는 사용후에 닫는다. close

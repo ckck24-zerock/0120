07e368ada7e4b8bc21bec519eefd55ceb04b4c4c
zerock <kangckck24@gmail.com>
2025-01-21 16:08:35 +0900

    2ㅜnd

--- src/main/java/org/example/ex4/HttpFTServer.java
+++ src/main/java/org/example/ex4/HttpFTServer.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class HttpFTServer {
 
@@ -16,10 +17,25 @@ public class HttpFTServer {
 
         System.out.println("FTserver run.............");
 
+        // 파일 데이터를 읽어서 outputStream에 write( )한다. - byte[] 이용하면 1000배 빨라짐
+        byte[] buffer = new byte[1024*8]; //8kb
+
         while(true){
 
             //브라우저와 연결해서 Socket을 생성해야 한다. - accept()
             Socket socket = serverSocket.accept();
+
+            InputStream in = socket.getInputStream();
+            Scanner scanner = new Scanner(in);
+
+            String first = scanner.nextLine();
+
+            if(!first.startsWith("GET /ring")){
+                scanner.close();
+                socket.close();
+
+                continue;
+            }
 
             //보내고 싶으니 OutputStream을 생성한다.
             OutputStream outputStream = socket.getOutputStream();
@@ -34,25 +50,21 @@ public class HttpFTServer {
             outputStream.write( ("Content-Length: "+target.length()+"\r\n").getBytes());
             outputStream.write("\r\n".getBytes());
 
-            // 파일 데이터를 읽어서 outputStream에 write( )한다. - byte[] 이용하면 1000배 빨라짐
-            byte[] buffer = new byte[1024*8]; //8kb
-
             while(true){
                 try {
                     int count = inputStream.read(buffer);//내용물은 buffer에 있고,
-
-                    System.out.println(count);
-
                     if (count == -1) {
                         break;
                     }
                     outputStream.write(buffer, 0, count);
                 }catch(Exception e){
                     System.out.println("Exception");
+                    break;
                 }
+
             }//end while
             outputStream.close();
-
+            socket.close();
         }//end while
     }
 
